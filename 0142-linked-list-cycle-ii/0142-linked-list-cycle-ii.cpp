3e0fcd9257ac6ce30451157fedf9c508ffbc62ee
Ashish kumar <132436213+Ashishkr8802@users.noreply.github.com>
2025-04-01 23:35:41 +0530

    Time: 8 ms (49.4%), Space: 8.1 MB (100%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,31 +9,35 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
+         //check for loop
+
         ListNode* slow = head;
-        ListNode * fast = head;
+        ListNode* fast = head;
 
         while(fast != NULL) {
-            fast = fast->next;
+            fast = fast -> next;
             if(fast != NULL) {
-                fast = fast->next;
-                slow = slow->next;
+                fast = fast -> next;
+                slow = slow -> next;
             }
 
             if(fast == slow) {
                 break;
             }
         }
-        if(fast == NULL) {
+
+        if(fast == NULL){
             return NULL;
         }
 
+        //yaha pahuche matlab slow and fast meet kar chuke hai
         slow = head;
 
         while(fast != slow) {
-            slow = slow->next;
-            fast = fast->next;
+            slow = slow -> next;
+            fast = fast -> next;
         }
 
         return slow;
-    }
+        }
 };
