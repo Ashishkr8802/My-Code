e4c69880176227c8f227912c60049ce461fa7621
Ashish kumar <132436213+Ashishkr8802@users.noreply.github.com>
2025-04-02 15:37:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -28,9 +28,9 @@ public:
             }
         }
 
-        for(int i=0 ; i<n ; i++) {
+        for(int i=1 ; i<=n ; i++) {
             if(nums[i] > 0) {
-                return i+1;
+                return i;
             }
         }
         return n+1;
