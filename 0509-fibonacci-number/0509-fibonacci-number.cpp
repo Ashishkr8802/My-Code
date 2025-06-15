40f593f002ef538c8c8ac43c9fa5738a19118a8e
Ashish kumar <132436213+Ashishkr8802@users.noreply.github.com>
2025-06-16 02:20:14 +0530

    Time: 0 ms (100%), Space: 8.3 MB (11.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,10 +1,21 @@
 class Solution {
 public:
-    int fib(int n) {
+    int solveUsingMemo(int n , vector<int> &dp) {
         if(n == 0 || n == 1) {
             return n;
         }
+        if(dp[n] != -1) {
+            return dp[n];
+        }
 
-        return fib(n-2) + fib(n-1);
+        dp[n] = solveUsingMemo(n-1 , dp) + solveUsingMemo(n-2 , dp);
+
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n+1 , -1);
+        int ans = solveUsingMemo(n , dp);
+
+        return ans;
     }
 };
