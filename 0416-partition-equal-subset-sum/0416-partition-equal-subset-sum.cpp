67cf77468022bc77e93826ea6624473fa97ca652
Ashish kumar <132436213+Ashishkr8802@users.noreply.github.com>
2025-08-01 15:52:44 +0530

    Time: 175 ms (19.59%), Space: 111.4 MB (14.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,30 +1,63 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-        int sum = 0;
-
-        for(auto i : nums) {
-            sum += i;
-        }
-
-        if(sum % 2 != 0) {
+    bool solveUsingRecursion(vector<int>& nums , int index , int target , int sum) {
+        if(index >= nums.size() || sum > target) {
             return false;
         }
 
-        int targetSum = sum/2;
-        
-        vector<bool> dp(targetSum+1 , false);
-        dp[0] = true;
-
-        for(int num : nums) {
-            for(int sum = targetSum ; sum>=num ; sum--) {
-                dp[sum] = dp[sum] || dp[sum - num];
-                if(dp[targetSum]) {
-                    return true;
-                }
-            }
+        if(sum == target) {
+            return true;
         }
-        return dp[targetSum];
+
+        bool include = solveUsingRecursion(nums , index+1 , target , sum+nums[index]);
+        bool exclude = solveUsingRecursion(nums , index+1 , target , sum);
+
+        return include||exclude;
+    }
+
+
+    bool solveUsingMemo(vector<int>& nums , int index , int target , int sum ,  vector<vector<int>> &dp) {
+        if(index >= nums.size() || sum > target) {
+            return false;
+        }
+
+        if(sum == target) {
+            return true;
+        }
+
+        if(dp[index][sum] != -1) {
+            return dp[index][sum];
+        }
+
+        bool include = solveUsingMemo(nums , index+1 , target , sum+nums[index] , dp);
+        bool exclude = solveUsingMemo(nums , index+1 , target , sum ,dp);
+
+        dp[index][sum] = (include||exclude);
+
+        return dp[index][sum];
+    }
+
+
+    bool canPartition(vector<int>& nums) {
+        int totalsum  = 0;
+        int index = 0;
+        int n = nums.size();
+
+        for(int i=0 ; i<n ; i++) {
+            totalsum += nums[i];
+        }
+
+        if(totalsum %2 != 0) {
+            return false;
+        }
+        int sum=0;
+
+        int target = totalsum/2;
+
+        vector<vector<int>> dp(n+1 , vector<int>(target+1 , -1));
+
+        // bool ans = solveUsingRecursion(nums , index , target , sum);
+        bool ans = solveUsingMemo(nums , index , target , sum , dp);
+        return ans;
     }
 };
