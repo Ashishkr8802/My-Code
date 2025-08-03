a3e5e217968c96d78a8cadba9082290d3fa002cc
Ashish kumar <132436213+Ashishkr8802@users.noreply.github.com>
2025-08-03 22:35:12 +0530

    Time: 3 ms (5.96%), Space: 10.8 MB (5.74%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,40 +1,44 @@
 class Solution {
 public:
-    int solveUsingRecursion(vector<int>& nums , int index) {
-        //base case
-        if(index >= nums.size()) {
+    // int solveUsingRecursion(vector<int>& nums , int idx) {
+    //     if(idx >= nums.size()) {
+    //         return 0;
+    //     }
+
+    //     int include = nums[idx] + solveUsingRecursion(nums , idx+2);
+    //     int exclude = 0 + solveUsingRecursion(nums , idx+1);
+
+    //     int ans = max(include , exclude);
+
+    //     return ans;
+    // }
+
+    int solveUsingMemo(vector<int>& nums , int idx , vector<int> &dp) {
+        if(idx >= nums.size()) {
             return 0;
         }
 
-        //include
-        int include = nums[index] + solveUsingRecursion(nums , index+2);
-        //exclude
-        int exclude = 0 + solveUsingRecursion(nums , index+1);
-
-        return max(include , exclude);
-    }
-
-    int solveUsingMemo(vector<int> &nums , int index , vector<int> &dp) {
-        if(index >= nums.size()) {
-            return 0;
+        if(dp[idx] != -1) {
+            return dp[idx];
         }
 
-        if(dp[index] != -1) {
-            return dp[index];
-        }
+        int include = nums[idx] + solveUsingMemo(nums , idx+2 , dp);
+        int exclude = solveUsingMemo(nums , idx+1 , dp);
 
-        int include = nums[index] + solveUsingMemo(nums , index+2 ,dp);
-        int exclude = 0 + solveUsingMemo(nums , index+1 , dp);
+        int ans = max(include , exclude);
 
-        dp[index] = max(include , exclude);
-        return dp[index];
+        dp[idx] = ans;
+
+        return ans;
     }
+
+
     int rob(vector<int>& nums) {
-        int index = 0;
-        // int ans = solveUsingRecursion(nums , index);
         int n = nums.size();
+        int idx = 0;
         vector<int> dp(n+1 , -1);
-        int ans = solveUsingMemo(nums , index , dp);
+        // int ans = solveUsingRecursion(nums , idx);
+        int ans = solveUsingMemo(nums , idx , dp);
         return ans;
     }
 };
